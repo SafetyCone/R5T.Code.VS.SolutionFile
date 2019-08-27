f19ce2993ea35c1e0170b0bab077f2acc3368c08
MinexAutomation <minex.automation@gmail.com>
2019-08-27 11:25:01 -0400

    Added code from R5T.Code.VisualStudio.SolutionFile.

---
+++ source/R5T.Code.VS.SolutionFile/Code/Extensions/SolutionFileExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using SolutionUtilitiesExtra = R5T.Code.VisualStudio.Model.SolutionFileSpecific.UtilitiesExtra;
+
+
+namespace R5T.Code.VisualStudio.Model
+{
+    public static class SolutionFileExtensions
+    {
+        /// <summary>
+        /// Adds a project file and all its dependencies to a solution in the dependencies solution folder.
+        /// </summary>
+        public static void AddProjectReferenceDependencyAndAllDependenciesChecked(this SolutionFile solutionFile, string solutionFilePath, string dependencyProjectFilePath)
+        {
+            SolutionUtilitiesExtra.AddProjectReferenceDependencyAndAllDependenciesChecked(solutionFile, solutionFilePath, dependencyProjectFilePath);
+        }
+    }
+}
---
+++ source/R5T.Code.VS.SolutionFile/Code/UtilitiesExtra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using ProjectFileUtilities = R5T.Code.VisualStudio.ProjectFile.Types.Utilities;
+
+
+namespace R5T.Code.VisualStudio.Model.SolutionFileSpecific
+{
+    public static class UtilitiesExtra
+    {
+        /// <summary>
+        /// Adds a project file and all its dependencies to a solution in the dependencies solution folder.
+        /// </summary>
+        public static void AddProjectReferenceDependencyAndAllDependenciesChecked(string solutionFilePath, string dependencyProjectFilePath)
+        {
+            var solutionFile = SolutionFile.Load(solutionFilePath);
+
+            UtilitiesExtra.AddProjectReferenceDependencyAndAllDependenciesChecked(solutionFile, solutionFilePath, dependencyProjectFilePath);
+
+            solutionFile.Save(solutionFilePath);
+        }
+
+        /// <summary>
+        /// Adds a project file and all its dependencies to a solution in the dependencies solution folder.
+        /// </summary>
+        public static void AddProjectReferenceDependencyAndAllDependenciesChecked(SolutionFile solutionFile, string solutionFilePath, string dependencyProjectFilePath)
+        {
+            var dependencyProjectFilePaths = ProjectFileUtilities.GetProjectReferenceDependencyFilePathsRecursive(dependencyProjectFilePath);
+
+            solutionFile.AddProjectReferenceDependencyAndAllDependenciesChecked(solutionFilePath, dependencyProjectFilePath, dependencyProjectFilePaths);
+        }
+    }
+}
